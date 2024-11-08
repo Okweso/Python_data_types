693d7807f0c000a915e61f89f53fe73068de0f59
Okweso <93729120+Okweso@users.noreply.github.com>
2024-11-08 16:18:38 +0300

    Delete user_input.py

--- user_input.py
+++
@@ -1,4 +0,0 @@
-name = input("Please enter your name: ")
-age = input('Please enter your age: ')
-location = input('What is your location? ')
-print(f"Hi {name} you are {age} years old and your current location is {location}")
